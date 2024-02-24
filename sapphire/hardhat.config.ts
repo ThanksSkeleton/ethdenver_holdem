27b8bba8802665380694b4aecc6d4483eb59a1f0
Thanks Skeleton <thanks.skeleton@gmail.com>
2024-02-24 15:23:11 -0700

    sapphire

---
+++ sapphire/hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import '@oasisprotocol/sapphire-hardhat';
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+    networks: {
+      'sapphire-testnet': {
+        // This is Testnet! If you want Mainnet, add a new network config item.
+        url: "https://testnet.sapphire.oasis.dev",
+        accounts: process.env.PRIVATE_KEY
+          ? [process.env.PRIVATE_KEY]
+          : [],
+        chainId: 0x5aff,
+      },
+    },
+};
+
+export default config;
