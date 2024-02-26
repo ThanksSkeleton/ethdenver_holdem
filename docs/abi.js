680c730f90434885f7391bdced09f26e1fc4e885
Dominic Letz <dominic.letz@berlin.de>
2024-02-25 21:35:01 -0700

    query chips

--- docs/abi.js
+++ docs/abi.js
@@ -2,14 +2,18 @@ const POKER = "0x4c4B3671c42c6E780ec5799A1612d8D5305ff6cd";
 const TOKEN = "0x39d61f9540D84D5E6920B06e973CEE19D79FeFbD";
 
 var ethers;
+var MaxUint256;
 import("https://cdn.jsdelivr.net/npm/ethers@6.11.1/+esm").then((mod) => {
     ethers = mod;
+    MaxUint256 = ethers.MaxUint256;
 });
 
 async function PokerContract(provider) {
     provider = new ethers.BrowserProvider(window.ethereum);
 
     abi = [
+        "function chips(address player, uint256 tableId) public view returns (uint256)",
+        "function buyIn(uint256 tableId, uint256 amount)",
         "function tables(uint256) public view returns (tuple(uint8 state, uint256 totalHands, uint256 currentRound, uint256 buyInAmount, uint256 maxPlayers, uint256 pot, uint256 bigBlind, uint256 token))",
         "function totalTables() public view returns (uint256)",
         "function createTable(uint256 buyInAmount, uint256 maxPlayers, uint256 bigBlind, address token)",
@@ -23,6 +27,8 @@ async function TokenContract(provider) {
 
     abi = [
         "function balanceOf(address) public view returns (uint256)",
+        "function allowance(address owner, address spender) public view returns (uint256)",
+        "function approve(address spender, uint256 value) returns (bool)",
     ];
     const signer = await provider.getSigner();
     return new ethers.Contract(TOKEN, abi, signer);
--- docs/vue/new_game.vue
+++ docs/vue/new_game.vue
@@ -1,82 +1,107 @@
 <template id="new_game">
-<div class="overflow-hidden px-3 py-10 flex justify-around">
-  <div class="w-full max-w-xs">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="player_count">
-          Player Count
-        </label>
-        <input 
-          v-model.trim="player_count"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="player_count" type="text" placeholder="Player Count">
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="buy_in">
-          Buy In
-        </label>
-        <input 
-          v-model.trim="buy_in"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="buy_in" type="text" placeholder="Buy In Value">
-      </div>
-      <div class="flex items-center justify-between">
-        <button v-on:click="create_game()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-          Create Table
-        </button>
-        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-          Need help?
-        </a>
-      </div>
-    </form>
-  </div>
-  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div class="px-4 sm:px-6 lg:px-8">
-      <div class="sm:flex sm:items-center">
-        <div class="sm:flex-auto">
-          <h1 class="text-base font-semibold leading-6 text-gray-900">Tables</h1>
-          <p class="mt-2 text-sm text-gray-700">You've got (<% balance %>) FISH tokens that you can play with on any of the open tables.</p>
+  <div class="overflow-hidden px-3 py-10 flex justify-around">
+    <div class="w-full max-w-xs">
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="player_count">
+            Player Count
+          </label>
+          <input v-model.trim="player_count"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="player_count" type="text" placeholder="Player Count">
         </div>
-      </div>
-      <div class="mt-8 flow-root">
-        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <table class="min-w-full divide-y divide-gray-300">
-              <thead>
-                <tr>
-                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Num</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">State</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Total Hands</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Current Round</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Buy In</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Max Players</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Pot</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Big Blind</th>
-                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                    <span class="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody class="divide-y divide-gray-200">
-                <tr v-for="table in tables">
-                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0"><% table.index %></td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><% table.state %></td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><% table.totalHands %></td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><% table.currentRound %></td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><% table.buyInAmount %></td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><% table.maxPlayers %></td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><% table.pot %></td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><% table.bigBlind %></td>
-                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Join Table</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="buy_in">
+            Buy In
+          </label>
+          <input v-model.trim="buy_in"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="buy_in" type="text" placeholder="Buy In Value">
+        </div>
+        <div class="flex items-center justify-between">
+          <button v-on:click="create_game()"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button">
+            Create Table
+          </button>
+          <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            Need help?
+          </a>
+        </div>
+      </form>
+    </div>
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+          <div class="sm:flex-auto">
+            <h1 class="text-base font-semibold leading-6 text-gray-900">Tables</h1>
+            <p class="mt-2 text-sm text-gray-700">You've got (<% balance %>) FISH tokens that you can play with on any of
+                the open tables.</p>
+          </div>
+        </div>
+        <div class="mt-8 flow-root">
+          <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <table class="min-w-full divide-y divide-gray-300">
+                <thead>
+                  <tr>
+                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Num
+                    </th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">State</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Total Hands</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Current Round</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Buy In</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Max Players</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Pot</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Big Blind</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Chips</th>
+                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200">
+                  <tr v-for="table in tables">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                      <% table.index %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.state %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.totalHands %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.currentRound %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.buyInAmount %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.maxPlayers %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.pot %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.bigBlind %>
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <% table.chips %>
+                    </td>
+                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a v-on:click="join_game(table.index)" href="#" class="text-indigo-600 hover:text-indigo-900">Join
+                        Table</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 <script>
 
@@ -96,38 +121,60 @@ var NewGameComponent = Vue.component("NewGame", {
   created: async function () {
     console.log("created");
     try {
-        await MMSDK.connect();
-        this.provider = await MMSDK.getProvider();
-        const res = await this.provider.request({
-          method: 'eth_requestAccounts',
-          params: [],
+      await MMSDK.connect();
+      this.provider = await MMSDK.getProvider();
+      const res = await this.provider.request({
+        method: 'eth_requestAccounts',
+        params: [],
+      });
+      this.account = res[0];
+      console.log('request accounts', res);
+      this.lastResponse = '';
+      await this.add_chain();
+      this.token = await TokenContract(this.provider);
+      this.balance = await this.token.balanceOf(this.account);
+      this.contract = await PokerContract(this.provider);
+      let totalTables = await this.contract.totalTables();
+      for (let i = 0; i < totalTables; i++) {
+        const table = await this.contract.tables(i);
+        let chips = await this.contract.chips(this.account, i);
+        this.tables.push({
+          index: i, state: table.state,
+          totalHands: table.totalHands, currentRound: table.currentRound,
+          buyInAmount: table.buyInAmount, maxPlayers: table.maxPlayers, pot: table.pot,
+          bigBlind: table.bigBlind, token: table.token,
+          chips: chips
         });
-        this.account = res[0];
-        console.log('request accounts', res);
-        this.lastResponse = '';
-        await this.add_chain();
-        this.token = await TokenContract(this.provider);
-        this.balance = await this.token.balanceOf(this.account);
-        this.contract = await PokerContract(this.provider);
-        let totalTables = await this.contract.totalTables();
-        for (let i = 0; i < totalTables; i++) {
-          const table = await this.contract.tables(i);
-          this.tables.push({index: i, state: table.state, totalHands: table.totalHands, currentRound: table.currentRound, buyInAmount: table.buyInAmount, maxPlayers: table.maxPlayers, pot: table.pot, bigBlind: table.bigBlind, token: table.token});
-        }
-      } catch (e) {
-        console.log('create ERR', e);
-      }      
+      }
+    } catch (e) {
+      console.log('create ERR', e);
+    }
   },
   methods: {
-    create_game: async function() {
+    create_game: async function () {
       console.log("create_game");
       try {
         await this.contract.createTable(this.buy_in, this.player_count, 1, TOKEN);
       } catch (e) {
         console.log('request accounts ERR', e);
-      }      
+      }
     },
-    add_chain: async function() {
+    join_game: async function (num) {
+      console.log("join_game", num);
+      let table = this.tables[num];
+      try {
+        let allowance = await this.token.allowance(this.account, POKER);
+        if (allowance < table.buyInAmount) {
+          let ok = await this.token.approve(POKER, MaxUint256);
+          if (ok) await this.contract.buyIn(num, table.buyInAmount);
+        } else {
+          await this.contract.buyIn(num, table.buyInAmount);
+        }
+      } catch (e) {
+        console.log('join_game ERR', e);
+      }
+    },
+    add_chain: async function () {
       try {
         const res = await this.provider.request({
           method: 'wallet_addEthereumChain',
