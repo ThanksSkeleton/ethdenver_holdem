ea079dc6b2e38d5431ec099ce31657836c6f95db
Dominic Letz <dominic.letz@berlin.de>
2024-02-25 19:04:47 -0700

    create table call

---
+++ docs/abi.js
@@ -0,0 +1,21 @@
+const POKER = "0x4c4B3671c42c6E780ec5799A1612d8D5305ff6cd";
+const TOKEN = "0x39d61f9540D84D5E6920B06e973CEE19D79FeFbD";
+
+var ethers;
+import("https://cdn.jsdelivr.net/npm/ethers@6.11.1/+esm").then((mod) => {
+    ethers = mod;
+});
+
+async function PokerContract(provider) {
+    // doCall(web3, pokerMethods[name], POKER, args, callback)
+    // let provider = new ethers.BrowserProvider(window.ethereum); 
+    // const signer = await provider.getSigner();
+
+    provider = new ethers.BrowserProvider(window.ethereum);
+
+    abi = [
+        "function createTable(uint256 buyInAmount, uint256 maxPlayers, uint256 bigBlind, address token)",
+    ];
+    const signer = await provider.getSigner();
+    return new ethers.Contract(POKER, abi, signer);
+}
