488c1bd691439e77bd236409fdc2391414c6be97
Thanks Skeleton <thanks.skeleton@gmail.com>
2024-02-25 17:56:45 -0700

    keccak encryption working

--- sapphire/contracts/Vigil.sol
+++ sapphire/contracts/Vigil.sol
@@ -18,11 +18,40 @@ contract Vigil {
     mapping(uint => uint[]) private communityCards_Private;
     mapping(uint => uint[]) public communityCards_Public;
 
+    event Deoxsys_Encrypt(
+        bytes nonce, 
+        bytes encrypted
+    );
+
+    event Keccak_Encrypt( 
+        bytes encrypted
+    );
 
     struct PlayerCards {
         uint8 card1;
         uint8 card2;
     }
+
+    function deoxsys_hello_world() public returns (bytes memory)
+    {
+        bytes32 key = "hello" ;
+        bytes32 nonce = keccak256(abi.encodePacked(block.timestamp, msg.sender));
+        bytes memory text = "secret_text_whale_hello";
+        bytes memory ad = "";
+        bytes memory encrypted =  Sapphire.encrypt(key, nonce, text, ad);
+        emit Deoxsys_Encrypt(abi.encodePacked(nonce), encrypted);
+        return encrypted;
+    }
+
+    function keccak_hello_world() public returns (bytes memory)
+    {
+        uint8 card = 32;
+        bytes32 secretKey = 0x123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0;
+        bytes memory encrypted = abi.encodePacked(keccak256(abi.encodePacked(secretKey, card)));
+        emit Keccak_Encrypt( encrypted);
+        return encrypted;
+    }
+
 
     // // Encrypted Get Psudeocode and Notes
 
@@ -93,82 +122,82 @@ contract Vigil {
         return false;
     }
 
-    struct SecretMetadata {
-        address creator;
-        string name;
-        /// @notice How long (in seconds) the secret should remain so past the creator's last update.
-        uint256 longevity;
-    }
+    // struct SecretMetadata {
+    //     address creator;
+    //     string name;
+    //     /// @notice How long (in seconds) the secret should remain so past the creator's last update.
+    //     uint256 longevity;
+    // }
 
-    event SecretCreated(
-        address indexed creator,
-        string indexed name,
-        uint256 index
-    );
-    event SecretRevealed(
-        address indexed creator,
-        string indexed name,
-        uint256 index
-    );
+    // event SecretCreated(
+    //     address indexed creator,
+    //     string indexed name,
+    //     uint256 index
+    // );
+    // event SecretRevealed(
+    //     address indexed creator,
+    //     string indexed name,
+    //     uint256 index
+    // );
 
-    SecretMetadata[] public _metas;
-    bytes[] private _secrets;
-    /// @dev The unix timestamp at which the address was last seen.
-    mapping(address => uint256) public _lastSeen;
+    // SecretMetadata[] public _metas;
+    // bytes[] private _secrets;
+    // /// @dev The unix timestamp at which the address was last seen.
+    // mapping(address => uint256) public _lastSeen;
 
-    function createSecret(
-        string calldata name,
-        uint256 longevity,
-        bytes calldata secret
-    ) external {
-        _updateLastSeen();
-        _metas.push(
-            SecretMetadata({
-                creator: msg.sender,
-                name: name,
-                longevity: longevity
-            })
-        );
-        _secrets.push(secret);
-        emit SecretCreated(msg.sender, name, _metas.length - 1);
-    }
+    // function createSecret(
+    //     string calldata name,
+    //     uint256 longevity,
+    //     bytes calldata secret
+    // ) external {
+    //     _updateLastSeen();
+    //     _metas.push(
+    //         SecretMetadata({
+    //             creator: msg.sender,
+    //             name: name,
+    //             longevity: longevity
+    //         })
+    //     );
+    //     _secrets.push(secret);
+    //     emit SecretCreated(msg.sender, name, _metas.length - 1);
+    // }
 
-    /// @notice Reveals the secret at the specified index.
-    function revealSecret(uint256 index) external returns (bytes memory) {
-        require(index < _metas.length, "no such secret");
-        address creator = _metas[index].creator;
-        uint256 expiry = _lastSeen[creator] + _metas[index].longevity;
-        require(block.timestamp >= expiry, "not expired");
-        emit SecretRevealed(creator, _metas[index].name, index);
-        return _secrets[index];
-    }
+    // /// @notice Reveals the secret at the specified index.
+    // function revealSecret(uint256 index) external returns (bytes memory) {
+    //     require(index < _metas.length, "no such secret");
+    //     address creator = _metas[index].creator;
+    //     uint256 expiry = _lastSeen[creator] + _metas[index].longevity;
+    //     require(block.timestamp >= expiry, "not expired");
+    //     emit SecretRevealed(creator, _metas[index].name, index);
+    //     return _secrets[index];
+    // }
 
-    /// @notice Returns the time (in seconds since the epoch) at which the owner was last seen, or zero if never seen.
-    function getLastSeen(address owner) external view returns (uint256) {
-        return _lastSeen[owner];
-    }
+    // /// @notice Returns the time (in seconds since the epoch) at which the owner was last seen, or zero if never seen.
+    // function getLastSeen(address owner) external view returns (uint256) {
+    //     return _lastSeen[owner];
+    // }
 
-    function getMetas(uint256 offset, uint256 count)
-        external
-        view
-        returns (SecretMetadata[] memory)
-    {
-        if (offset >= _metas.length) return new SecretMetadata[](0);
-        uint256 c = offset + count <= _metas.length
-            ? count
-            : _metas.length - offset;
-        SecretMetadata[] memory metas = new SecretMetadata[](c);
-        for (uint256 i = 0; i < c; ++i) {
-            metas[i] = _metas[offset + i];
-        }
-        return metas;
-    }
+    // function getMetas(uint256 offset, uint256 count)
+    //     external
+    //     view
+    //     returns (SecretMetadata[] memory)
+    // {
+    //     if (offset >= _metas.length) return new SecretMetadata[](0);
+    //     uint256 c = offset + count <= _metas.length
+    //         ? count
+    //         : _metas.length - offset;
+    //     SecretMetadata[] memory metas = new SecretMetadata[](c);
+    //     for (uint256 i = 0; i < c; ++i) {
+    //         metas[i] = _metas[offset + i];
+    //     }
+    //     return metas;
+    // }
 
-    function refreshSecrets() external {
-        _updateLastSeen();
-    }
+    // function refreshSecrets() external {
+    //     _updateLastSeen();
+    // }
 
-    function _updateLastSeen() internal {
-        _lastSeen[msg.sender] = block.timestamp;
-    }
+    // function _updateLastSeen() internal {
+    //     _lastSeen[msg.sender] = block.timestamp;
+    // }
 }
