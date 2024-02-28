78c9f43a3ef1fea57c7952fa28651b7d53a1538e
Thanks Skeleton <thanks.skeleton@gmail.com>
2024-02-28 13:27:17 -0700

    poker hand validation pure functions including TODOs

--- sapphire/contracts/PokerHandValidation.sol
+++ sapphire/contracts/PokerHandValidation.sol
@@ -3,7 +3,119 @@ pragma solidity ^0.8.0;
 
 // ChatGPT is helping me a lot with this
 
-contract PokerHandValidation {
+contract PokerHandValidation { // TODO - convert to library to help bytecode size
+
+    // Solidity Work
+    // User is going to claim they have a poker hand
+    // (HAND_TYPE, uint[5] indexes)
+    // You get from the table
+    // uint[7] cards
+    // Step one - Obvious verification -
+    //  cards are as described
+    //  right number of cards
+    //  any other obvious stuff
+    // Step 2- Hand Check
+    // Make sure the HAND TYPE is correct
+    // Every hand needs a canonical ordering and the player needs to give it in canonical ordering
+    // Step 3 - Pairwise Ranking
+    // Given two hands, which one beats?
+    // Step 4 - Multi Ranking 
+    // of a list of players, which one is the best
+
+    // Unit tests for all of these
+    
+    // Client Side Code Work
+    // Given a list of 7 cards, identify the indexes that make the best hand
+    // (Pre existing library? Linked in discord) 
+
+    enum HandType {
+        HighCard,       // Lowest value hand
+        OnePair,        // Two cards of the same value
+        TwoPair,        // Two different pairs
+        ThreeOfAKind,   // Three cards of the same value
+        Straight,       // All five cards in consecutive value order
+        Flush,          // All five cards of the same suit, not in sequence
+        FullHouse,      // Three of a kind with a pair
+        FourOfAKind,    // Four cards of the same value
+        StraightFlush,  // Five cards in sequence, all of the same suit
+        RoyalFlush      // Ten, Jack, Queen, King, Ace, in the same suit
+    }
+
+	struct ShowdownHand_HeadToHead 
+	{
+		address player;		
+		HandType handType;
+		uint[5] actualCards; // Those same cards (the actual cards) in canonical order
+	}
+
+    function handCardsExist(
+        uint[7] memory availableCards,
+        uint[5] memory cardIndexes,
+        uint[5] memory myCards
+    ) public pure returns (bool) {
+        for (uint i = 0; i < cardIndexes.length; i++) {
+            if (availableCards[cardIndexes[i]] != myCards[i]) {
+                return false; // If any card does not match, return false
+            }
+        }
+        return true; // If all cards match, return true
+    }
+	
+    function HandRecognize(HandType handType, uint[5] memory cards) public pure returns (bool) {
+        // TODO: Implement the logic to verify if the hand matches the handType
+        // Assume It's truthful for now
+        
+        return true; // Placeholder return statement
+    }
+	
+    function DetermineWinners(ShowdownHand_HeadToHead[] memory allhands) public returns (address[] memory) {
+        // rank hands by HandType, then by internal cards (by the specific rule for that handtype)
+        
+        ShowdownHand_HeadToHead[] memory bestHandsByType = BestHandTypes(allhands); // Corrected variable declaration
+        if (bestHandsByType.length == 1) { // Corrected conditional syntax
+            address[] memory winners = new address[](1); // Correct way to initialize an array with a single address
+            winners[0] = bestHandsByType[0].player; // Assuming .player is the correct field name for the address
+            return winners;
+        } 
+        
+        return BestHand_SameTypes(bestHandsByType); 
+    }
+	
+    function BestHandTypes(ShowdownHand_HeadToHead[] memory allhands) public pure returns (ShowdownHand_HeadToHead[] memory) {
+        HandType highestHandType = HandType.HighCard; // Start with the lowest possible value
+        uint count = 0;
+
+        // First, find the highest HandType value
+        for (uint i = 0; i < allhands.length; i++) {
+            if (allhands[i].handType > highestHandType) {
+                highestHandType = allhands[i].handType;
+                count = 1; // Reset count for new highest type
+            } else if (allhands[i].handType == highestHandType) {
+                count++; // Increment count for matching highest type
+            }
+        }
+
+        // Then, collect all hands of that HandType
+        ShowdownHand_HeadToHead[] memory bestHands = new ShowdownHand_HeadToHead[](count);
+        uint index = 0;
+        for (uint i = 0; i < allhands.length; i++) {
+            if (allhands[i].handType == highestHandType) {
+                bestHands[index] = allhands[i];
+                index++;
+            }
+        }
+
+        return bestHands;
+    }
+
+    function BestHand_SameTypes(ShowdownHand_HeadToHead[] memory contender_hands) public pure returns (address[] memory) {
+        // TODO: We need to compare multiple hands of the same type.
+        // for now just select the first player as a single winner
+
+        address[] memory winner = new address[](1); // Create a new dynamic array with 1 address element
+        winner[0] = contender_hands[0].player; // Assign the first hand's player address to the first element of the winner array
+        return winner; // Return the array containing the address
+    }
 
     // Rank Represenation
 
@@ -69,63 +181,5 @@ contract PokerHandValidation {
         }
         return true;
     }
-    
-    // Solidity Work
-    // User is going to claim they have a poker hand
-    // (HAND_TYPE, uint[5] indexes)
-    // You get from the table
-    // uint[7] cards
-    // Step one - Obvious verification -
-    //  cards are as described
-    //  right number of cards
-    //  any other obvious stuff
-    // Step 2- Hand Check
-    // Make sure the HAND TYPE is correct
-    // Every hand needs a canonical ordering and the player needs to give it in canonical ordering
-    // Step 3 - Pairwise Ranking
-    // Given two hands, which one beats?
-    // Step 4 - Multi Ranking 
-    // of a list of players, which one is the best
-
-    // Unit tests for all of these
-    
-    // Client Side Code Work
-    // Given a list of 7 cards, identify the indexes that make the best hand
-    // (Pre existing library? Linked in discord) 
-
-
-	// function HandCardsExist(uint[7] availableCards, uint[5] cardIndexes, uint[5] mycards) -> bool 
-	// {
-	// 	// confirm that for each card index, availablecards[cardIndexes[i]] == mycards[i]] 
-	// }
-	
-	// function HandRecognize(HandType handType, uint[5] cards) -> bool //This hand is actually as described
-	// {
-	// 	// TODO
-	// 	return true;
-	// }
-	
-	// function StartShowdown(ShowdownHand_HeadToHead[] allhands) -> address[] // return the winners
-	// { 
-	// 	// rank hands by HandType, then by internal cards (by the specific rule for that handtype)
-		
-	// 	besthandsbyType ShowdownHand_HeadToHead[] = BestHandTypes(allhands);
-	// 	if bestHandsbyType.length == 1 
-	// 	{
-	// 		return bestHandsbyType[0].address;
-	// 	} 
-		
-	// 	return BestHand_SameTypes(besthandsbyType);
-	// }
-	
-	// function BestHandTypes(ShowdownHand_HeadToHead[] allhands) -> ShowdownHand_HeadToHead[] 
-	// {
-	
-	// }
-	
-	// function BestHand_SameTypes(ShowdownHand_HeadToHead[] contender_hands) -> address[] // HANDLE TIES?
-	// {
-	
-	// }
 
 }
---
+++ sapphire/test/pokerhandvalidationtests.ts
@@ -0,0 +1,97 @@
+
+// Usage: pnpm hardhat run --network <network> scripts/run-vigil.ts
+
+import {
+    time,
+    loadFixture,
+  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+  import chai = require("chai");
+  import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+  import { expect } from "chai";
+  import { ethers } from "hardhat";
+  import { Poker, PokerToken } from "../typechain-types/contracts";
+  import { ContractFactory, Contract, Signer } from "ethers";
+  const { hash_decrypt_card, decrypt_hole_cards } = require('../scripts/decrypt_from_salt.js');
+  
+  
+  
+  import chaiAsPromised = require("chai-as-promised");
+  
+  chai.use(chaiAsPromised);
+  chai.config.includeStack = true;
+  
+  describe('Poker Hand Validation Tests', () => {
+    let poker : Poker;
+    let poker_token: PokerToken;
+  
+    let creator : Signer;
+  
+    let player1 : Signer;
+    let player2 : Signer;
+    let player3 : Signer;
+    let player4 : Signer;
+  
+    let stranger : Signer;
+  
+    let two_player_game_players : Signer[];
+    let four_player_game_players : Signer[];
+  
+  
+    const MINIMUM_BUY_IN_AMOUNT = 200;
+    const TWO_PLAYERS = 2;
+    const FOUR_PLAYERS = 4;
+    const BIG_BLIND = 2;
+  
+    const TABLE_ID = 0;
+    const BUY_IN_AMOUNT = 300;
+    
+    const player1_salt = 1;
+    const player2_salt = 2;
+    const player3_salt = 3;
+    const player4_salt = 4;
+  
+    const BETTING_ROUND_PREFLOP = 0;
+    const BETTING_ROUND_FLOP = 1;
+    const BETTING_ROUND_TURN = 2;
+    const BETTING_ROUND_RIVER = 3;
+  
+    const PLAYER_ACTION_CALL = 0;
+    const PLAYER_ACTION_RAISE = 1;
+    const PLAYER_ACTION_CHECK = 2;
+    const PLAYER_ACTION_FOLD = 3;
+  
+    const HAND_ID = 0;
+  
+    beforeEach(async function () {
+  
+        // copy paste
+
+      const [crx, px1, px2, px3, px4, stx] = await ethers.getSigners();
+      creator = crx;
+      player1 = px1;
+      player2 = px2;
+      player3 = px3;
+      player4 = px4;
+      stranger = stx;
+  
+      two_player_game_players = [player1, player2];
+      four_player_game_players = [player1, player2, player3, player4]
+  
+      let factory1 = await ethers.getContractFactory('Poker');
+      poker = await factory1.deploy() as Poker;
+      await poker.waitForDeployment();
+  
+      let factory2 = await ethers.getContractFactory('PokerToken');
+      poker_token = await factory2.deploy(poker.getAddress()) as PokerToken;
+      await poker_token.waitForDeployment();
+  
+      poker_token.mint(await player1.getAddress(), 1000);
+      poker_token.mint(await player2.getAddress(), 1000);
+      poker_token.mint(await player3.getAddress(), 1000);    
+      poker_token.mint(await player4.getAddress(), 1000);    
+    });
+  
+    it("TODO", async function () {
+    });
+
+  });
